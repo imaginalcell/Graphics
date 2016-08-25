731ead1f68bc9fc31ac8fda1f6ae012796d1168c
triston.vaskey <triston.vaskey@SEA-E17155>
2016-08-25 14:03:57 -0700

    made a triangle and geomentry

--- RenderUtils/crenderutils.cpp
+++ RenderUtils/crenderutils.cpp
@@ -2,11 +2,49 @@
 
 #include "GLEW\glew.h"
 #include "GLFW\glfw3.h"
-
+#include "Vertex.h"
 #include "crenderutils.h"
 
-void test()
+Geomentry makeGeomentry(const Vertex * verts, size_t vsize, const unsigned int * tris, size_t tsize)
 {
-	glfwInit();
-	glfwTerminate();
+	Geomentry retval;
+	retval.size = tsize;
+
+	//define the var
+	glCreateBuffers(1, &retval.vbo);
+	glCreateBuffers(1, &retval.ibo);
+	glCreateVertexArrays(1, &retval.vao);
+	
+	//scope the var
+	glBindVertexArray(retval.vao);
+	glBindBuffer(GL_ARRAY_BUFFER, retval.vbo); //scope our vertices
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, retval.ibo);// scope triangles
+
+	//init the var
+		//new flaot [4]
+	glBufferData(GL_ARRAY_BUFFER, vsize * sizeof(Vertex), verts, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, tsize * sizeof(unsigned), tris, GL_STATIC_DRAW);
+
+	//attributes
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*)16);
+
+	//unscope the var
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER ,0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER ,0);
+	
+	return retval;
+}
+
+void freeGeomentry(Geomentry &geo)
+{
+	glDeleteBuffers(1, &geo.vbo);
+	glDeleteBuffers(1, &geo.ibo);
+	glDeleteVertexArrays(1, &geo.vao);
+
+	geo = { 0,0,0,0 };
 }
--- RenderUtils/crenderutils.h
+++ RenderUtils/crenderutils.h
@@ -1,2 +1,16 @@
 #pragma once
-void test();
+
+struct  Geomentry
+{
+	//vertex buffer object: array of vertices
+	//index buffer object : array of indeices (triangles)
+	//vertex arrat object : groups the two with some formatting
+	//size				  : number rof triangles
+
+	unsigned vao, vbo, ibo, size;
+};
+
+Geomentry makeGeomentry(const struct Vertex *verts, size_t vsize,
+						const unsigned int *tris, size_t tsize);
+
+void freeGeomontry(Geomentry &);
